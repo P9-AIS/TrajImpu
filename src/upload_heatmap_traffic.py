5c7a03ea963479c7fbdb693935cc5b51cc8b4ac0
William Woldum <41169797+williamwoldum@users.noreply.github.com>
2025-11-23 15:19:31 +0000

    Finetuned traffic heatmap and added upload

--- src/upload_heatmap_traffic.py
+++ src/upload_heatmap_traffic.py
@@ -4,14 +4,22 @@ from ForceData.force_data_access_handler_db import ForceDataAccessHandlerDb
 from ForceData.force_data_upload_handler_http import ForceDataUploadHandlerHTTP
 from ForceProviders.force_provider_traffic import TrafficForceProvider
 from ForceUtils.heatmap_generator import generate_heatmap_image
-
+from ForceUtils.reprojector import Reprojector
 
 if __name__ == "__main__":
     cfg = parse_config("config.yaml")
 
     data_handler = ForceDataAccessHandlerDb(PostgresConnection(cfg.postgresCfg))
     traffic_provider = TrafficForceProvider(data_handler, cfg.trafficForceProviderCfg)
-    image = generate_heatmap_image(traffic_provider.get_vectormap(), False, cfg.heatmapGeneratorCfg)
+    image_path_3034 = generate_heatmap_image(traffic_provider.get_vectormap(), cfg.heatmapGeneratorCfg)
+    image_path_3857 = image_path_3034.replace(".png", "_3857.png")
+
+    new_area = Reprojector.reproject_png_3034_to_3857(
+        image_path_3034,
+        image_path_3857,
+        cfg.trafficForceProviderCfg.area
+    )
 
     upload_handler = ForceDataUploadHandlerHTTP(cfg.forceDataUploadHandlerCfg)
-    upload_handler.upload_image_3034(image, "traffic_heatmap", cfg.trafficForceProviderCfg.area)
+    upload_handler.upload_image(image_path_3034, "traffic_heatmap_3034", cfg.trafficForceProviderCfg.area)
+    upload_handler.upload_image(image_path_3857, "traffic_heatmap_3857", new_area)
